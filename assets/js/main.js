f8d3d75f8717c0683ee9580cff96bf04f78c67b3
MD <dhmedy@googlemail.com>
2022-08-08 13:55:33 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// Array deutscheGerichte
+let deutscheGerichte = ["Thürigner Rostbratwurst", "Quarkkeulchen", "Sauerbraten", "Brezel", "Kartoffelsalat", "Kaiserschmarrn"];
+console.log(deutscheGerichte);
+// Entferne aus dem Array 3 Werte mit Hilfe der Methode shift();
+let gut = [deutscheGerichte.shift(), deutscheGerichte.shift(), deutscheGerichte.shift()];
+
+// Speichere diese als Array nichtGut. 
+
+console.log(gut);
+
+console.log(deutscheGerichte);
